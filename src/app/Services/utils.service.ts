a13805326a6274bb01ad0d88e788f9b3d31b0665
Isaac Yriarte <isaacyagi@gmail.com>
2024-10-20 18:26:17 -0500

    feat: add utils service

---
+++ src/app/Services/utils.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilsService {
+
+  constructor() { }
+
+  CSVToJSON(csvText: string): any[]{
+    const allLines = csvText.trim().split("\n");
+    const header = allLines[0]
+    const dataLines = allLines.slice(1)
+    const fieldNames = header.split(environment.config.templateDelimiter)
+
+    let objList = []
+
+    for (let i = 0; i < dataLines.length; i++){
+      let obj:any = {}
+      const data = dataLines[i].split(environment.config.templateDelimiter)
+      for (let j = 0; j < fieldNames.length; j++){
+        const fieldName = this.removeLineBreaks(fieldNames[j].toLowerCase().trim());
+        console.log(data[j])
+        obj[fieldName] = this.removeLineBreaks(data[j])
+      }
+      objList.push(obj)
+    }
+    return objList
+  }
+
+  removeLineBreaks(text: string): string{
+    return text?.replace(/\r?\n|\r/,"") || ""
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   config:{
     sleepBetweenMessagesInMs: 3000,
-    maxSizeMessage:65536
+    maxSizeMessage:65536,
+    templateDelimiter: ";"
   },
   wsApiUrl: "http://localhost:3000"
 };
